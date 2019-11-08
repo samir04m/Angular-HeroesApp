1883ab76b32dc271fb109908424fc0b04715b1d8
Samir Mejia <samir04m@gmail.com>
2019-11-08 01:11:56 -0500

    Creacion del servicio e interface heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroes/heroe.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { HeroesService } from './services/heroes.service';
 
 import { app_routing } from './app.routes';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -19,9 +22,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     app_routing
   ],
-  providers: [],
+  providers: [
+     HeroesService
+ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
